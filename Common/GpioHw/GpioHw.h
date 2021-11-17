2d793ca5bf21d1a5dc5b99accf30e3d2c39dda3e
ssitkowx <sitkowskis@interia.pl>
2021-11-17 14:38:29 +0100

    small refactor

--- Common/GpioHw/GpioHw.h
+++ Common/GpioHw/GpioHw.h
@@ -14,28 +14,22 @@
 //////////////////////////////// VARIABLES ////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
-static xQueueHandle queueHandle;
+using  GpioIsr = void (*) (void * v_arg);
 
 ///////////////////////////////////////////////////////////////////////////////
 /////////////////////////// CLASSES/STRUCTURES ////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-
-static void IRAM_ATTR isr (void * v_arg)
-{
-    uint32_t pinNum = (uint32_t) v_arg;
-    xQueueSendFromISR (queueHandle, &pinNum, NULL);
-}
 
 class GpioHw : public Gpio <GpioHw>
 {
     static constexpr char * const MODULE = (char *)"Gpio";
 
     public:
-        GpioHw ()
+        GpioHw (GpioIsr v_isrType)
         {
             gpio_install_isr_service (ESP_INTR_FLAG_LEVEL1);
-            setPinConfig             (GPIO_NUM_26, GPIO_INTR_POSEDGE, GPIO_MODE_INPUT , GPIO_PULLDOWN_DISABLE, GPIO_PULLUP_ENABLE);
-            setPinConfig             (GPIO_NUM_27, GPIO_INTR_DISABLE, GPIO_MODE_OUTPUT, GPIO_PULLDOWN_DISABLE, GPIO_PULLUP_DISABLE);
+            setPinConfig             (GPIO_NUM_26, v_isrType, GPIO_INTR_POSEDGE, GPIO_MODE_INPUT , GPIO_PULLDOWN_DISABLE, GPIO_PULLUP_ENABLE);
+            setPinConfig             (GPIO_NUM_27, v_isrType, GPIO_INTR_DISABLE, GPIO_MODE_OUTPUT, GPIO_PULLDOWN_DISABLE, GPIO_PULLUP_DISABLE);
 
         }
         ~GpioHw () = default;
@@ -44,7 +38,7 @@ class GpioHw : public Gpio <GpioHw>
         bool ReadPinLevel (const uint16_t v_num) { return (bool)gpio_get_level ((gpio_num_t)v_num); }
 
     private:
-        void setPinConfig (const gpio_num_t v_num, const gpio_int_type_t v_isrType, const gpio_mode_t v_mode, const gpio_pulldown_t v_pullDown, const gpio_pullup_t v_pullUp)
+        void setPinConfig (const gpio_num_t v_num, GpioIsr v_gpioIsr, const gpio_int_type_t v_isrType, const gpio_mode_t v_mode, const gpio_pulldown_t v_pullDown, const gpio_pullup_t v_pullUp)
         {
             gpio_config_t config = {};
             config.intr_type     = v_isrType;
@@ -53,7 +47,7 @@ class GpioHw : public Gpio <GpioHw>
             config.pull_down_en  = v_pullDown;
             config.pull_up_en    = v_pullUp;
             gpio_config          (&config);
-            gpio_isr_handler_add (v_num, isr, (void *)v_num);
+            gpio_isr_handler_add (v_num, v_gpioIsr, (void *)v_num);
         }
 };
 
