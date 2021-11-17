8488822efb07a9aaa95569fe89bda68301233ee9
ssitkowx <sitkowskis@interia.pl>
2021-11-17 12:38:04 +0100

    Update2

--- Common/GpioHw/GpioHw.h
+++ Common/GpioHw/GpioHw.h
@@ -11,24 +11,19 @@
 #include "freertos/queue.h"
 
 ///////////////////////////////////////////////////////////////////////////////
-/////////////////////////// CLASSES/STRUCTURES ////////////////////////////////
+//////////////////////////////// VARIABLES ////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
 static xQueueHandle queueHandle;
 
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////// CLASSES/STRUCTURES ////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
 static void IRAM_ATTR isr (void * v_arg)
 {
-    uint32_t num = (uint32_t) v_arg;
-    xQueueSendFromISR (queueHandle, &num, NULL);
-}
-
-static void gpio_task_example(void* arg)
-{
-    uint32_t pinNum;
-    while (1)
-    {
-        if (xQueueReceive(queueHandle, &pinNum, portMAX_DELAY)) { printf ("GPIO [%d] intr, val: %d\n", pinNum, gpio_get_level ((gpio_num_t)pinNum)); }
-    }
+    uint32_t pinNum = (uint32_t) v_arg;
+    xQueueSendFromISR (queueHandle, &pinNum, NULL);
 }
 
 class GpioHw : public Gpio <GpioHw>
@@ -41,9 +36,6 @@ class GpioHw : public Gpio <GpioHw>
             gpio_install_isr_service (ESP_INTR_FLAG_LEVEL1);
             setPinConfig             (GPIO_NUM_26, GPIO_INTR_POSEDGE, GPIO_MODE_INPUT , GPIO_PULLDOWN_DISABLE, GPIO_PULLUP_ENABLE);
             setPinConfig             (GPIO_NUM_27, GPIO_INTR_DISABLE, GPIO_MODE_OUTPUT, GPIO_PULLDOWN_DISABLE, GPIO_PULLUP_DISABLE);
-
-            queueHandle = xQueueCreate (10, sizeof (uint32_t));
-            xTaskCreate (gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
 
         }
         ~GpioHw () = default;
@@ -61,8 +53,6 @@ class GpioHw : public Gpio <GpioHw>
             config.pull_down_en  = v_pullDown;
             config.pull_up_en    = v_pullUp;
             gpio_config          (&config);
-            //gpio_install_isr_service (ESP_INTR_FLAG_LEVEL1);    // try remove this from here
-            //gpio_set_intr_type   (v_num, v_isrType);
             gpio_isr_handler_add (v_num, isr, (void *)v_num);
         }
 };
